1f7d30671c5f21ea836bb4bf36834ef357364697
Ryan Hurst <ryan@teem.com>
2020-01-09 15:04:29 -0700

    updated pullrequest report to commitComparisonReport

--- githubHelper.go
+++ githubHelper.go
@@ -10,21 +10,6 @@ import "github.com/google/go-github/github" // with go modules disabled
 var ctx context.Context
 var client *github.Client
 
-func main() {
-	setup("4ae373d0cc57fef0aa53463077599f5744bc1f30")
-
-	//createNewReleaseBranch("RyanHurstTeem", "TestRepo", "release-branch", "b057e74c9b3aeb59ca3f5456acee0232c11a7d99")
-	createPullRequest(
-		"RyanHurstTeem",
-		"TestRepo",
-		github.String("release-branch"),
-		github.String("master"),
-		github.String("test PR"),
-		github.String("this is the body of the test PR"),
-	)
-	//println(createReleaseDocument("enderlabs", "android", 538).String())
-}
-
 func setup(accessToken string) {
 	ctx = context.Background()
 	ts := oauth2.StaticTokenSource(
@@ -34,28 +19,34 @@ func setup(accessToken string) {
 	client = github.NewClient(tc)
 }
 
-func createReleaseDocument(owner string, repo string, pullRequestNumber int) PullRequestReport {
-	var pullRequestReport PullRequestReport
-	pullRequestReport.tickets = make(map[string][]*github.Commit)
-	pullRequest, _, _ := client.PullRequests.Get(ctx, owner, repo, pullRequestNumber)
-	if pullRequest != nil {
-		pullRequestReport.pullRequest = pullRequest
+//func createPullRequestReport(owner string, repo string, pullRequestNumber int) PullRequestReport {
+//	var pullRequestReport PullRequestReport
+//	pullRequest, _, _ := client.PullRequests.Get(ctx, owner, repo, pullRequestNumber)
+//	if pullRequest != nil {
+//		pullRequestReport.pullRequest = pullRequest
+//	}
+//	commits, _, err := client.PullRequests.ListCommits(ctx, owner, repo, pullRequestNumber, nil)
+//	if err != nil {
+//		println("ERROR comparing commits: ", err.Error())
+//		return pullRequestReport
+//	}
+//	pullRequestReport.tickets = createSomeShit(commits)
+//	return pullRequestReport
+//}
+
+func createCommitsComparisonReport(owner string, repo string, base string, head string) CommitsComparisonReport {
+	var commitsComparisonReport CommitsComparisonReport
+	commitsComparison, _, err := client.Repositories.CompareCommits(ctx, owner, repo, base, head)
+	if commitsComparison != nil {
+		commitsComparisonReport.commitsComparison = commitsComparison
 	}
-	commits, _, _ := client.PullRequests.ListCommits(ctx, owner, repo, pullRequestNumber, nil)
-	r, _ := regexp.Compile("(?i)teem-([0-9]+)")
-	if commits != nil {
-		for _, commit := range commits {
-			commitMessage := *commit.Commit.Message
-			ticketNumber := r.FindStringSubmatch(commitMessage)
-			if ticketNumber != nil {
-				if pullRequestReport.tickets[ticketNumber[1]] == nil {
-					pullRequestReport.tickets[ticketNumber[1]] = []*github.Commit{}
-				}
-				pullRequestReport.tickets[ticketNumber[1]] = append(pullRequestReport.tickets[ticketNumber[1]], commit.Commit)
-			}
-		}
+	if err != nil {
+		println("ERROR comparing commits: ", err.Error())
+		return commitsComparisonReport
 	}
-	return pullRequestReport
+
+	commitsComparisonReport.tickets = createSomeShit(commitsComparison.Commits)
+	return commitsComparisonReport
 }
 
 func createNewReleaseBranch(owner string, repo string, branchName string, sha string) *github.Reference{
@@ -78,12 +69,31 @@ func createNewReleaseBranch(owner string, repo string, branchName string, sha st
 	return returnedReference
 }
 
-func createPullRequest(owner string, repo string, head *string, base *string, title *string, body *string) *github.PullRequest {
+func createSomeShit(commits []github.RepositoryCommit) map[string][]*github.Commit {
+	ticketMap := make(map[string][]*github.Commit)
+	if commits == nil {
+		return ticketMap
+	}
+	r, _ := regexp.Compile("(?i)teem-([0-9]+)")
+	for _, commit := range commits {
+		commitMessage := *commit.Commit.Message
+		ticketNumber := r.FindStringSubmatch(commitMessage)
+		if ticketNumber != nil {
+			if ticketMap[ticketNumber[1]] == nil {
+				ticketMap[ticketNumber[1]] = []*github.Commit{}
+			}
+			ticketMap[ticketNumber[1]] = append(ticketMap[ticketNumber[1]], commit.Commit)
+		}
+	}
+	return ticketMap
+}
+
+func createPullRequest(owner string, repo string, base *string, head *string, title *string, body *string) *github.PullRequest {
 	pull := &github.NewPullRequest{
 		Title:               title,
 		Body:                body,
-		Head:                head,
 		Base:                base,
+		Head:                head,
 		Issue:               nil,
 		MaintainerCanModify: github.Bool(true),
 		Draft:               github.Bool(false),
@@ -95,9 +105,26 @@ func createPullRequest(owner string, repo string, head *string, base *string, ti
 	return pullRequest
 }
 
-func (pullRequestReport PullRequestReport) String() string {
-	var pullRequestString = *pullRequestReport.pullRequest.Title
-	for key, commits := range pullRequestReport.tickets {
+//func (pullRequestReport PullRequestReport) String() string {
+//	var pullRequestString = *pullRequestReport.pullRequest.Title
+//	for key, commits := range pullRequestReport.tickets {
+//		pullRequestString += "\n"
+//		pullRequestString += key
+//		for _, commit := range commits {
+//			pullRequestString += "\n    " + *commit.Message + " by: " + *commit.Author.Name
+//		}
+//	}
+//	return pullRequestString
+//}
+//
+//type PullRequestReport struct {
+//	pullRequest *github.PullRequest
+//	tickets     map[string][]*github.Commit
+//}
+
+func (commitsComparisonReport CommitsComparisonReport) String() string {
+	var pullRequestString = *commitsComparisonReport.commitsComparison.Status
+	for key, commits := range commitsComparisonReport.tickets {
 		pullRequestString += "\n"
 		pullRequestString += key
 		for _, commit := range commits {
@@ -107,7 +134,7 @@ func (pullRequestReport PullRequestReport) String() string {
 	return pullRequestString
 }
 
-type PullRequestReport struct {
-	pullRequest *github.PullRequest
+type CommitsComparisonReport struct {
+	commitsComparison *github.CommitsComparison
 	tickets     map[string][]*github.Commit
 }
